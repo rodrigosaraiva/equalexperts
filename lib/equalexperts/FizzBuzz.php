19a586affb3b449bb2de67be4b0468daeeaa783f
Rodrigo Saraiva <eu@rodrigosaraiva.com.br>
2016-05-30 21:00:29 +0100

    First Test

---
+++ lib/equalexperts/FizzBuzz.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rodrigo
+ * Date: 01-06-2016
+ * Time: 21:01
+ */
+
+namespace EqualExperts;
+
+
+abstract class FizzBuzz
+{
+    /**
+     * fizzBuzz
+     *
+     * @param integer $value
+     * @return string
+     */
+    public static function fizzBuzz($value)
+    {
+        return (string) $value;
+    }
+}
---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rodrigo
+ * Date: 01-06-2016
+ * Time: 20:46
+ */
+
+namespace Tests;
+
+use EqualExperts\FizzBuzz;
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test test_if_fizz_buzz_is_string
+     */
+    public function test_if_fizz_buzz_is_string() 
+    {
+        $this->assertEquals('1', FizzBuzz::fizzBuzz(1));
+    }
+}
