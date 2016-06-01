784a2cd6679118ad2bac47ebcc18207ee38bbd3a
Rodrigo Saraiva <eu@rodrigosaraiva.pt>
2016-06-01 22:26:26 +0100

    Program to test class

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo $baseDir;
+/**
+ * Created by PhpStorm.
+ * User: rodrigo
+ * Date: 01-06-2016
+ * Time: 22:20
+ */
+require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/equalexperts/FizzBuzz.php';
+
+use EqualExperts\FizzBuzz;
+
+for ($x=1;$x<=50;$x++) {
+    print_r(FizzBuzz::fizzBuzz($x) . "&nbsp;");
+}
